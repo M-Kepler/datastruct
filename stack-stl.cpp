f2351597f97f68caa1c55dfbc7a6e265a85edf41
Kepler_Linux <870131615@qq.com>
2016-04-11 16:46:30 +0800

    rename queue-link.cpp

--- stack-stl.cpp
+++ stack-stl.cpp
@@ -1,3 +1,12 @@
+/***********************************************************
+* Author       : M_Kepler
+* EMail        : hellohuangjinjie@gmail.com
+* Last modified: 2016-04-11 16:16:02
+* Filename     : stack-stl.cpp
+* Description  :
+    * push()\pop()\top()\empty()\size()
+**********************************************************/
+
 #include <stack>
 #include <iostream>
 using namespace std;
@@ -6,20 +15,17 @@ int main( )
 {
     int n;
     double item;
-    stack<double> numbers; // declares and initializes a stack of numbers
-    cout << "Type in an integer n followed by n decimal numbers."
-        << endl
-        << "The numbers will be printed in reverse order."
-        << endl;
+    stack<double> stack_stl; // declares and initializes a stack of numbers
+    cout << "Type in an integer n followed by n decimal numbers.\n";
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> item;
-        numbers.push(item);
+        stack_stl.push(item);
     }
-    cout << endl << endl;
-    while (!numbers.empty( )) {
-        cout << numbers.top( ) << " ";
-        numbers.pop( );
+    cout << endl;
+    while (!stack_stl.empty( )) {
+        cout << stack_stl.top( ) << " ";
+        stack_stl.pop( );
     }
     cout << endl;
 }
